cb6f24ccaf4806246b921ad61ec4cf700f69aa28
lotamim <tamim.iubat@gmail.com>
2021-01-09 00:07:11 +0600

    change

--- src/components/constant/Constants.js
+++ src/components/constant/Constants.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:8090/";
+// export const API_URL = "http://localhost:8090/";
+export const API_URL = "https://application-hrm.herokuapp.com/";
+// https://application-hrm.herokuapp.com/
